f746793a9befcb5118cbf391919a5cffa8ad42e6
Joe Devine <joedevine@Joes-Apollo-MacBook.local>
2022-11-14 11:34:58 +0000

    Update gitignore

--- telemetry-reciever/build/routes/posts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = __importDefault(require("express"));
-const posts_1 = __importDefault(require("../controllers/posts"));
-const router = express_1.default.Router();
-router.post("/posts", posts_1.default.addPost);
-module.exports = router;
