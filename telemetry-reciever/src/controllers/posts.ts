3d702edbe4a05c55fc9934fe4c7b5c1294ece263
Joe Devine <joedevine@Joes-Apollo-MacBook.local>
2022-11-14 11:33:53 +0000

    Add template node server for telemetry collection

---
+++ telemetry-reciever/src/controllers/posts.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import axios, { AxiosResponse } from "axios";
+
+interface Post {
+  userId: Number;
+  id: Number;
+  title: String;
+  body: String;
+}
+
+// let posts: [Post] = result.data;
+
+// adding a post
+const addPost = async (req: Request, res: Response, next: NextFunction) => {
+  let posts: Post = req.body;
+  // add the post
+  let response: AxiosResponse = await axios.post(
+    `https://jsonplaceholder.typicode.com/posts`,
+    posts
+  );
+  // return response
+  return res.status(200).json({
+    message: response.data,
+  });
+};
+
+export default { addPost };
